4541f69161f118feae05ed5cdc030cfa182c9856
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-07-24 11:24:41 -0700

    Add media query

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import ChangeUnitContainer from '../containers/ChangeUnitContainer'
 
  export default function App () {
   return (
-    <div className="container clearfix" id="main">
+    <div className="container" id="main">
       <CurrentTempContainer />
       <ForecastContainer />
     </div>
--- src/components/CurrentTemp.js
+++ src/components/CurrentTemp.js
@@ -5,14 +5,15 @@ import ChangeUnitContainer from '../containers/ChangeUnitContainer';
 function CurrentTemp ({currentTemp, unit, location}) {
   if (Object.keys(currentTemp).length === 0) {
     return (
-      <div className="currentTemp">
-        <p className="todayText">Today</p>
+      <div className="currentTempBox">
+        <div className="currentTemp">
+          <p className="todayText">Today</p>
+        </div>
       </div>
     )
   }
   return (
-    <div>
-
+    <div className="currentTempBox">
       <div className="currentTemp">
         <p className="todayText">Today</p>
         <h1>{location}</h1>
